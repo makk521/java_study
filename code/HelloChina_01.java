a370983ca6a82ec1621e9d6831bf8c6814d0d3cf
makaka <75128685+makk521@users.noreply.github.com>
2023-07-19 21:38:52 +0800

    add chapter 1
    学习并添加chapter_01的笔记与代码

---
+++ code/HelloChina_01.java
@@ -0,0 +1,16 @@
+/*
+ * @author makk521
+ * @version 1.0
+ */
+public class HelloChina_01{
+    //create main method
+    public static void main(String[] args){
+        System.out.println("hello world 你好!");
+    }
+}
+
+class HelloDog{
+	public static void main(String[] args){
+		System.out.println("Hello Dog!");
+	}
+}
